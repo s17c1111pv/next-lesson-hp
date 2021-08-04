7b384fdf748808be32b266a628dbe40c86461651
s17c1111pv <88127845+s17c1111pv@users.noreply.github.com>
2021-08-04 17:54:54 +0900

    initial comit

---
+++ app-hp/pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout"
+
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <p className="text-4xl">Hello Nextjs</p>
+    </Layout>
+  )
+}
