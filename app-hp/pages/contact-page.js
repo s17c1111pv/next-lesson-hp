b929b0a5a58375d8e2c5490f86104ef3ddad8afe
s17c1111pv <88127845+s17c1111pv@users.noreply.github.com>
2021-08-05 10:32:05 +0900

    change to function

--- app-hp/pages/contact-page.js
+++ app-hp/pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import Image from "next/image";
 
-const Contact = () => {
+export default function Contact () {
     return (
         <Layout>
             <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
@@ -80,4 +80,3 @@ const Contact = () => {
         </Layout>
     );
 }
-export default Contact;
